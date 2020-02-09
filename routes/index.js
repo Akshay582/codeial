3c980430bf9ab6f418b632667b39f06fbfa826c3
Akshay Mittal <akshaymittal.da@gmail.com>
2020-02-09 11:38:56 +0530

    Next starting with the EJS.

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ const homeController = require('../controllers/home_controller');
 console.log('The router is not fucked.');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// from any further routes access comes from here
+// router.use('/routerName', require('./routerfile'))
 
 module.exports = router;
